7be008e6d274c6092ccd1e0d735b1dc9dd76cb9f
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-24 00:41:36 +0530

    in.close() with try finally

--- balancedBrackets2.java
+++ balancedBrackets2.java
@@ -4,43 +4,44 @@ class balancedBrackets {
     public static void main(String[] args) {
 
         Scanner scn = new Scanner(System.in);
+        try {
+            String str = scn.nextLine();
 
-        String str = scn.nextLine();
+            Stack<Character> st = new Stack<>();
 
-        Stack<Character> st = new Stack<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            } else if (ch == ')') {
-                boolean val = handleClosing(st, '(');
-                if (val == false) {
-                    System.out.println(val);
-                    return;
-                }
-            } else if (ch == '}') {
-                boolean val = handleClosing(st, '{');
-                if (val == false) {
-                    System.out.println(val);
-                    return;
-                }
-            } else if (ch == ']') {
-                boolean val = handleClosing(st, '[');
-                if (val == false) {
-                    System.out.println(val);
-                    return;
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                if (ch == '(' || ch == '{' || ch == '[') {
+                    st.push(ch);
+                } else if (ch == ')') {
+                    boolean val = handleClosing(st, '(');
+                    if (val == false) {
+                        System.out.println(val);
+                        return;
+                    }
+                } else if (ch == '}') {
+                    boolean val = handleClosing(st, '{');
+                    if (val == false) {
+                        System.out.println(val);
+                        return;
+                    }
+                } else if (ch == ']') {
+                    boolean val = handleClosing(st, '[');
+                    if (val == false) {
+                        System.out.println(val);
+                        return;
+                    }
                 }
             }
+            // opening bracket jyada honge
+            if (st.size() > 0) {
+                System.out.println(false);
+            } else {// pura bracket over sab khatam
+                System.out.println(true);
+            }
+        } finally {
+            scn.close();
         }
-        // opening bracket jyada honge
-        if (st.size() > 0) {
-            System.out.println(false);
-        } else {// pura bracket over sab khatam
-            System.out.println(true);
-        }
-
-        scn.close();
     }
 
     public static boolean handleClosing(Stack<Character> st, char ch) {
